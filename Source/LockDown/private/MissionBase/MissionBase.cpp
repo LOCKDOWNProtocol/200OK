d6c294b2a4f3d953cee30dbbc64cd2eb1b554bc6
ez23re <ez23re@gmail.com>
2025-04-10 18:01:52 +0900

    미션액터만들기만해둠

---
+++ Source/LockDown/private/MissionBase/MissionBase.cpp
@@ -0,0 +1,20 @@
+﻿#include "MissionBase/MissionBase.h"
+
+AMissionBase::AMissionBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AMissionBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AMissionBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
